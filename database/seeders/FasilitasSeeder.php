d7d53483593c1d1725b119315841fa8b093068e4
Kelompok Purnama <purnamamarasabessy11@gmail.com>
2026-07-07 15:12:48 +0900

    perbaikan

--- database/seeders/FasilitasSeeder.php
+++ database/seeders/FasilitasSeeder.php
@@ -12,13 +12,13 @@ class FasilitasSeeder extends Seeder
         $data = [
             // GRATIS
             ['nama' => 'Parkir', 'deskripsi' => 'Area parkir luas untuk motor dan mobil.', 'tipe' => 'gratis', 'icon' => '🅿️', 'harga' => null, 'satuan_harga' => null, 'urutan' => 1],
-            ['nama' => 'Musholah', 'deskripsi' => 'Tempat ibadah yang bersih dan nyaman.', 'tipe' => 'gratis', 'icon' => '🕌', 'harga' => null, 'satuan_harga' => null, 'urutan' => 2],
+            ['nama' => 'Mushola,', 'deskripsi' => 'Tempat ibadah yang bersih dan nyaman.', 'tipe' => 'gratis', 'icon' => '🕌', 'harga' => null, 'satuan_harga' => null, 'urutan' => 2],
 
             // BERBAYAR
             ['nama' => 'Warung Makan', 'deskripsi' => 'Rujak buah, kelapa muda, mie, dan camilan lainnya.', 'tipe' => 'bayar', 'icon' => '🍽️', 'harga' => null, 'satuan_harga' => null, 'urutan' => 3],
             ['nama' => 'Toilet', 'deskripsi' => 'Toilet umum tersedia di area pantai.', 'tipe' => 'bayar', 'icon' => '🚻', 'harga' => 3000, 'satuan_harga' => '/ pemakaian', 'urutan' => 4],
             ['nama' => 'Rumah Adat', 'deskripsi' => 'Sewa rumah adat khas Maluku untuk bersantai bersama keluarga.', 'tipe' => 'bayar', 'icon' => '🏠', 'harga' => 250000, 'satuan_harga' => '/ hari', 'urutan' => 5],
-            ['nama' => 'Tarpal', 'deskripsi' => 'Alas duduk santai di tepi pantai.', 'tipe' => 'bayar', 'icon' => '🟦', 'harga' => 30000, 'satuan_harga' => '/ hari', 'urutan' => 6],
+            ['nama' => 'Terpal', 'deskripsi' => 'Alas duduk santai di tepi pantai.', 'tipe' => 'bayar', 'icon' => '🟦', 'harga' => 30000, 'satuan_harga' => '/ hari', 'urutan' => 6],
             ['nama' => 'Bantal Renang', 'deskripsi' => 'Pelampung bantal untuk anak-anak dan dewasa.', 'tipe' => 'bayar', 'icon' => '🛟', 'harga' => 5000, 'satuan_harga' => '/ hari', 'urutan' => 7],
             ['nama' => 'Air Tawar', 'deskripsi' => 'Air tawar untuk bilas setelah berenang.', 'tipe' => 'bayar', 'icon' => '💧', 'harga' => 5000, 'satuan_harga' => '/ jerigen', 'urutan' => 8],
 
